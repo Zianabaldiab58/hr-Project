818f07bc91a5f03321bc74d0cde0038a51169de1
unknown <diabzainab55@gmail.com>
2024-06-20 18:39:13 +0300

    Initial commit with ProposalAges program

---
+++ ProposalAges.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ProposalAges {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of proposals: ");
+        int numProposals = scanner.nextInt();
+        int[] ages = new int[numProposals];
+        System.out.println("Enter the ages of people who made proposals:");
+        for (int i = 0; i < numProposals; i++) {
+            System.out.print("Age " + (i + 1) + ": ");
+            ages[i] = scanner.nextInt();
+        }scanner.close();
+        printAges(ages);}
+
+    public static void printAges(int[] ages) {
+        System.out.println("\nAges of people who made proposals:");
+        for (int age : ages) {
+            System.out.println(age + " years old");
+        }
+    }}
